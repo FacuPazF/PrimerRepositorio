88538f3016d991341525c7431e8bf4bb891ad8be
FacuPazF <facupazfessia@gmail.com>
2020-08-05 11:13:40 -0300

    Proyecto realizado el día 30 de Julio 2020 sobre un formulario de login

---
+++ MiPrimerProyecto/MiPrimerProyecto/frmLogin.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace MiPrimerProyecto
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtClave = new System.Windows.Forms.TextBox();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.lblPasswd = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.Location = new System.Drawing.Point(89, 208);
+            this.btnIngresar.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(66, 40);
+            this.btnIngresar.TabIndex = 2;
+            this.btnIngresar.Text = "Ingresar";
+            this.btnIngresar.UseVisualStyleBackColor = true;
+            this.btnIngresar.Click += new System.EventHandler(this.btnIngresar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(208, 208);
+            this.btnSalir.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(66, 40);
+            this.btnSalir.TabIndex = 3;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(162, 82);
+            this.txtUsuario.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(132, 20);
+            this.txtUsuario.TabIndex = 0;
+            // 
+            // txtClave
+            // 
+            this.txtClave.Location = new System.Drawing.Point(162, 121);
+            this.txtClave.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.txtClave.Name = "txtClave";
+            this.txtClave.Size = new System.Drawing.Size(132, 20);
+            this.txtClave.TabIndex = 1;
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Location = new System.Drawing.Point(52, 82);
+            this.lblUsuario.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(101, 13);
+            this.lblUsuario.TabIndex = 5;
+            this.lblUsuario.Text = "Nombre de Usuario:";
+            // 
+            // lblPasswd
+            // 
+            this.lblPasswd.AutoSize = true;
+            this.lblPasswd.Location = new System.Drawing.Point(86, 121);
+            this.lblPasswd.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.lblPasswd.Name = "lblPasswd";
+            this.lblPasswd.Size = new System.Drawing.Size(64, 13);
+            this.lblPasswd.TabIndex = 4;
+            this.lblPasswd.Text = "Contraseña:";
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(355, 328);
+            this.Controls.Add(this.lblPasswd);
+            this.Controls.Add(this.lblUsuario);
+            this.Controls.Add(this.txtClave);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnIngresar);
+            this.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmLogin";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnIngresar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.TextBox txtClave;
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.Label lblPasswd;
+    }
+}
+
---
+++ MiPrimerProyecto/MiPrimerProyecto/frmLogin.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiPrimerProyecto
+{
+    public partial class frmLogin : Form
+    {
+        string user = "admin";
+        string pw = "1234";
+
+
+
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            if (this.txtUsuario.Text == string.Empty)
+            {
+                MessageBox.Show("Debe ingresar Usuario...   ");
+                this.txtUsuario.Focus();
+                return;
+            };
+            //if (this.txtClave.Text == "")
+            if (string.IsNullOrEmpty(this.txtClave.Text))
+            {
+                MessageBox.Show("Debe Ingresar Clave...");
+                this.txtClave.Focus();
+                return;
+            }
+
+            // Validar Nombre de Usuario y Clave
+            string msg = "";
+
+            /*if (this.txtUsuario.Text == this.user && this.txtClave.Text == this.pw)
+                msg = "Login Ok";
+            else
+                msg = "Usuario y/o clave incorrectos";
+            */
+            if (this.txtUsuario.Text == this.user && this.txtClave.Text == this.pw)
+            {
+                msg = "Login Ok";
+                MessageBox.Show(msg, "Ingreso al Sistema ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                
+
+            }
+            else
+            {
+                msg = "Usuario y/o clave incorrectos";
+                MessageBox.Show(msg, "Ingreso al Sistema ", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.txtUsuario.Text = string.Empty;
+                this.txtClave.Text = string.Empty;
+                this.txtUsuario.Focus();
+            };
+                
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
